0f38648f60c580ae1e2704057fb8997b30da6d80
Carlos Manuel Fraile Gomez <carlosfrailegomez93@gmail.com>
2022-10-20 16:35:52 +0200

    17

--- CleanCode/03-functions.ts
+++ CleanCode/03-functions.ts
@@ -38,6 +38,42 @@
 
     function createActor(actor:any){}
 
+    // CONTINUACION : DETALLES ADICIONALES SOBRE FUNCIONES :
+
+    /* [X]
+    const getPayAmount = ({ isDead = false, isSeparated = true, isRetired = false }) => {
+        let result;
+        if ( isDead ) {
+            result = 1500;
+        } else {
+            if ( isSeparated ) {
+                result = 2500;
+            } else {
+                if ( isRetired ) {
+                    result = 3000;
+                } else {
+                    result = 4000; 
+                }
+            }
+        }
+        
+        return result;
+    } */
+
+    interface employeeCondiciton {isDead:boolean,isSeparated:boolean,isRetired:boolean};
+    const getPayAmount = ({isDead = false , isSeparated = true , isRetired = false}:employeeCondiciton):number =>
+    /*
+    (isDead) ? 1500 :
+    (isSeparated) ? 2500 :
+    (isRetired) ? 3000 : 4000
+    */
+    {
+        if(isDead)return 1500;
+        if(isSeparated)return 2500;
+        //if(isRetired){return 3000};return 4000}
+        return isRetired ? 3000 : 4000
+    }
+
 })();
 
 
--- app.ts
+++ app.ts
@@ -1,2 +1,12 @@
 console.clear();
-console.log('hola mundo');
+
+interface employeeCondiciton {isDead:boolean,isSeparated:boolean,isRetired:boolean};
+const getPayAmount = ({isDead = false , isSeparated = true , isRetired = false}:employeeCondiciton):number =>
+/*
+(isDead) ? 1500 :
+(isSeparated) ? 2500 :
+(isRetired) ? 3000 : 4000
+*/
+{if(isDead){return 1500};if(isSeparated){return 2500};if(isRetired){return 3000};return 4000}
+
+console.log(getPayAmount({isDead:false,isSeparated:true,isRetired:false}))
