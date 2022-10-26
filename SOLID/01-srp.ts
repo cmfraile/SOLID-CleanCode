6349d8e87387492fdf601bd371233f756aa4abfc
Carlos Manuel Fraile Gomez <carlosfrailegomez93@gmail.com>
2022-10-26 10:31:16 +0200

    vamos a resolver por libre el ejercicio de Liskov, aplicando O y L de SOLID tal y como desea el pofe

---
+++ SOLID/01-srp.ts
@@ -0,0 +1,38 @@
+interface Product {id:number,name:string}
+
+// Usualmente, esto es una clase para controlar la vista que es desplegada al usuario
+// Recuerden que podemos tener muchas vistas que realicen este mismo trabajo.
+class ProductBloc {
+
+    loadProduct( id: number ) {
+        // Realiza un proceso para obtener el producto y retornarlo
+        console.log('Producto: ',{ id, name: 'OLED Tv' });
+    }
+
+    saveProduct( product: Product ) {
+        // Realiza una petición para salvar en base de datos 
+        console.log('Guardando en base de datos', product );
+    }
+
+    notifyClients() {
+        console.log('Enviando correo a los clientes');
+    }
+
+    onAddToCart( productId: number ) {
+        // Agregar al carrito de compras
+        console.log('Agregando al carrito ', productId );
+    }
+    
+}
+
+
+
+export const solid01_SRP_main = () => {
+    const productBloc = new ProductBloc();
+    productBloc.loadProduct(10);
+    productBloc.saveProduct({ id: 10, name: 'OLED TV' });
+    productBloc.notifyClients();
+    productBloc.onAddToCart(10);
+}
+
+
--- app.ts
+++ app.ts
@@ -1,9 +1,11 @@
-import { homework4 } from './CleanCode/04-functionTarea';
-import { drymain } from './CleanCode/05-dry';
+//import { homework4 } from './CleanCode/04-functionTarea';
+//import { drymain } from './CleanCode/05-dry';
 //import { classes01main } from './CleanCode/06-classes01';
-import { classes02main } from './CleanCode/06-classes02';
-import { tarea07 } from './CleanCode/07-tarea';
+//import { classes02main } from './CleanCode/06-classes02';
+//import { tarea07 } from './CleanCode/07-tarea';
+//import { solid01_SRP_main } from './SOLID/01-srp';
+import { LiskovHomeworkMain } from './SOLID/03-liskov_a';
 
 console.clear();
 
-tarea07();
+LiskovHomeworkMain();
